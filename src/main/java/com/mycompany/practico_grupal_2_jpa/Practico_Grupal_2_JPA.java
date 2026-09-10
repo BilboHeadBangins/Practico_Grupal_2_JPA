f731719b3771036ee0495180dea0855fa40bb089
BilboHeadBangins <italoleonardo24345@gmail.com>
2026-09-09 22:40:33 -0300

    creación del proyecto

---
+++ src/main/java/com/mycompany/practico_grupal_2_jpa/Practico_Grupal_2_JPA.java
@@ -0,0 +1,12 @@
+package com.mycompany.practico_grupal_2_jpa;
+
+/**
+ *
+ * @author italo
+ */
+public class Practico_Grupal_2_JPA {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
